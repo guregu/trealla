3702d4abd157b4ff4d834d0976876c41396dd3d7
Greg <greg@toki.waseda.jp>
2023-03-02 19:19:45 +0900

    release .wasms unzipped
    (need a direct link for Spin)

--- src/contrib.c
+++ src/contrib.c
@@ -1,11 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-#include <ctype.h>
-#include <math.h>
-#include <float.h>
-#include <errno.h>
 
 #include "trealla.h"
 #include "internal.h"
