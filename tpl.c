9d99eba9b6982edbe98faec2453615f8519f0927
Greg <greg@toki.waseda.jp>
2023-04-23 18:49:55 +0900

    tweak pl_query test

--- tpl.c
+++ tpl.c
@@ -374,7 +374,7 @@ int main(int ac, char *av[], char * envp[])
 
 		g_tpl_interrupt = 0;
 
-#if 0
+#if 1
 		pl_eval(pl, src);
 #else
 		pl_sub_query *subq;
@@ -382,9 +382,8 @@ int main(int ac, char *av[], char * envp[])
 		int32_t out_len, err_len;
 		pl_query_captured(pl, src, &subq, &out, &out_len, &err, &err_len, 0);
 		do {
-			printf("x: %s\ny: %s\n", out, err);
+			printf("stdout: %s\nstderr: %s\n", out, err);
 		} while (pl_redo_captured(subq, &out, &out_len, &err, &err_len));
-		// pl_done(subq);
 #endif
 
 		free(line);
