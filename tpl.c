d069e54cda4ffd68c4d90ceeaa605a1de0c9b250
Greg <greg@toki.waseda.jp>
2023-05-04 00:57:16 +0900

    build fix

--- tpl.c
+++ tpl.c
@@ -374,16 +374,16 @@ int main(int ac, char *av[], char * envp[])
 
 		g_tpl_interrupt = 0;
 
-#if 1
+#if 0
 		pl_eval(pl, src);
 #else
 		pl_sub_query *subq;
 		char *out, *err;
 		int32_t out_len, err_len;
-		pl_query_captured(pl, src, &subq, &out, &out_len, &err, &err_len, 0);
+		pl_query(pl, src, &subq, 0);
 		do {
-			printf("stdout: %s\nstderr: %s\n", out, err);
-		} while (pl_redo_captured(subq, &out, &out_len, &err, &err_len));
+			//printf("stdout: %s\nstderr: %s\n", out, err);
+		} while (pl_redo(subq));
 #endif
 
 		free(line);
